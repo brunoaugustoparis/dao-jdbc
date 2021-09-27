4b0f6bfba80c51433c9d97954ea72a925708746c
Bruno Augusto Paris <bruno.pariss@hotmail.com>
2021-09-27 02:26:23 -0300

    Classe Seller

--- src/aplication/Program.java
+++ src/aplication/Program.java
@@ -1,6 +1,9 @@
 package aplication;
 
+import java.util.Date;
+
 import entities.Department;
+import entities.Seller;
 
 public class Program {
 
@@ -8,7 +11,10 @@ public class Program {
 
 
 		Department obj = new Department(1, "Books");
-		System.out.println(obj);
+		
+		Seller seller = new Seller(21, "Bob", "bob@gmail.com", new Date(), 3000.0, obj);
+		
+		System.out.println(seller);
 
 	}
 
